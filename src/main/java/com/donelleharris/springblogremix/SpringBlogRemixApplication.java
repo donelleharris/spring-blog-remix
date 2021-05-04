b3b5c132e78e6e8b7f7efd249f6c3f5e33a25b9a
Donelle Harris <donelle.harris@gmail.com>
2021-05-03 22:31:29 -0500

    Initial MVC / Spring framework file structure created

---
+++ src/main/java/com/donelleharris/springblogremix/SpringBlogRemixApplication.java
@@ -0,0 +1,13 @@
+package com.donelleharris.springblogremix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogRemixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogRemixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donelleharris/springblogremix/SpringBlogRemixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donelleharris.springblogremix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRemixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
